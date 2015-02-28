5009ee0b8f7cf98d81aeb0d12b601921dafc6ab6
Ozzie Ahmad <ahmad_ao2214@hotmail.com>
2015-02-28 06:31:52 -0600

    Map is up

---
+++ app/src/main/java/edu/depaul/cdm/css/photolrecall/activity/MapActivity.java
@@ -0,0 +1,33 @@
+package edu.depaul.cdm.css.photolrecall.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import edu.depaul.cdm.css.photolrecall.R;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+
+/**
+ * Created by OZ on 2/28/2015.
+ */
+
+
+public class MapActivity extends Activity implements GoogleApiClient.ConnectionCallbacks{
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_photo_map);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+}
